7d9acf4ca22395b8429a4230a4d5a96ebbd07d2c
annv <annv>
2021-05-03 23:36:22 +0700

    firstcm

---
+++ src/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Annv\Calculator;
+
+class Calculator
+{
+	public static function tong($a, $b)
+	{
+		return $a + $b;
+	}
+
+	public static function hieu($a, $b)
+	{
+		return $a - $b;
+	}
+}
